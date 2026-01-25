e3e0473ae18aa25e2b6287f937e07ff0bf1eeff6
BrajMohan <Brajmohanmadhuriyaanshu@gmail.com>
2026-01-25 17:50:31 +0530

    Part 0 = Initial Commit

---
+++ src/test/java/CodingB/StaySavvy/StaySavvyApplicationTests.java
@@ -0,0 +1,13 @@
+package CodingB.StaySavvy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaySavvyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
