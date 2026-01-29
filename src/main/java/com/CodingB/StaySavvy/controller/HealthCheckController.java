914be36b1badd4f5d2feabb960440b2b5ec04829
BrajMohan <Brajmohanmadhuriyaanshu@gmail.com>
2026-01-29 17:05:36 +0530

    added buildspec.yml file

--- src/main/java/com/CodingB/StaySavvy/controller/HealthCheckController.java
+++ src/main/java/com/CodingB/StaySavvy/controller/HealthCheckController.java
@@ -3,13 +3,14 @@ package com.CodingB.StaySavvy.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class HealthCheckController {
 
     @GetMapping("/")
-    public ResponseEntity<String> healthCheckController(){
-        return ResponseEntity.ok("ok");
+    public ResponseEntity<Map<String, String>> healthCheckController(){
+        // Returning a Map avoids the String casting error
+        return ResponseEntity.ok(Map.of("message", "ok"));
     }
-
 }
